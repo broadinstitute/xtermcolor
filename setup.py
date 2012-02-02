191e8cec83a205076751f9b68ad7b4a92f02df42
Scott Frazer <scott.d.frazer@gmail.com>
2012-02-02 17:20:55 -0500

    v1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.0.0'
+version = '1.0.1'
 README = os.path.join(os.path.dirname(__file__), 'README')
 long_description = open(README).read()
 
