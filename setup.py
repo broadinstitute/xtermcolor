9d027de20a7ead9e5058561562edc4e33f9bac34
Scott Frazer <scott.d.frazer@gmail.com>
2012-02-02 08:03:25 -0500

    Adding MIT license and classifiers

--- setup.py
+++ setup.py
@@ -19,8 +19,21 @@ setup(
       'xtermcolor = xtermcolor.Main:Cli'
     ]
   },
-  license = "GPL",
+  license = "MIT",
   keywords = "xterm, ANSI, xterm-256, terminal, color",
   url = "http://github.com/broadinstitute/xtermcolor",
-  classifiers=[]
+  classifiers=[
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.0',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Terminals :: Terminal Emulators/X Terminals',
+    'Topic :: Utilities'
+  ]
 )
