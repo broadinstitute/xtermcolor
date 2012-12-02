006e3e351ba131fa5015c7b7abdadfcf76d67981
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-12-02 15:32:01 +0100

    - That function had become useless

--- xtermcolor/__init__.py
+++ xtermcolor/__init__.py
@@ -2,29 +2,6 @@ from os import isatty, environ, write
 
 from xtermcolor.ColorMap import XTermColorMap, VT100ColorMap
 
-def _colorize(string, rgb=None, ansi=None,bg=None,ansi_bg=None,xterm=True):
-    '''Returns the colored string to print on the terminal.
-    
-    string = the string to print. Only accepts strings, unicode strings must
-             be encoded in advance.
-    rgb    = Rgb color for the text; for example 0xFF0000 is red.
-    ansi   = Ansi for the text
-    bg     = Rgb color for the background
-    ansi_bg= Ansi color for the background
-    xterm  = True by default, if False, escape codes for VT100 terminal will
-             be generated.
-    '''
-    if xterm != _colorize.xterm:
-        _colorize.xterm = xterm
-        if xterm:
-            _colorize.cmap = XTermColorMap()
-        else:
-            _colorize.cmap = VT100ColorMap()
-    return _colorize.cmap.colorize(string,rgb,ansi,bg,ansi_bg)
-
-_colorize.xterm = True
-_colorize.cmap = XTermColorMap()
-
 def colorize(string, rgb=None, ansi=None,bg=None,ansi_bg=None,fd=1):
     '''Returns the colored string to print on the terminal.
     
@@ -52,16 +29,16 @@ def colorize(string, rgb=None, ansi=None,bg=None,ansi_bg=None,fd=1):
         colorize.is_term = isatty(fd)
         if 'TERM' in environ:
             if environ['TERM'] == 'xterm':
-                colorize.xterm = True
+                colorize.cmap = XTermColorMap()
             elif environ['TERM'] == 'vt100':
-                colorize.xterm = False
+                colorize.xterm = VT100ColorMap()
             else:
                 colorize.is_term = False
         else:
             colorize.is_term = False
     
     if colorize.is_term:
-        string=_colorize(string,rgb,ansi,bg,ansi_bg,colorize.xterm)
+        string = colorize.cmap.colorize(string,rgb,ansi,bg,ansi_bg)
 
     return string
 colorize.init = False
