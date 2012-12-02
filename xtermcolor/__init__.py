97d2bb393a322db71e785882ffc671ed7f6699c7
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-12-02 14:57:28 +0100

    Added some documentation

--- xtermcolor/__init__.py
+++ xtermcolor/__init__.py
@@ -1,7 +1,17 @@
 from xtermcolor.ColorMap import XTermColorMap, VT100ColorMap
 
-
 def colorize(string, rgb=None, ansi=None,bg=None,ansi_bg=None,xterm=True):
+    '''Returns the colored string to print on the terminal.
+    
+    string = the string to print. Only accepts strings, unicode strings must
+             be encoded in advance.
+    rgb    = Rgb color for the text; for example 0xFF0000 is red.
+    ansi   = Ansi for the text
+    bg     = Rgb color for the background
+    ansi_bg= Ansi color for the background
+    xterm  = True by default, if False, escape codes for VT100 terminal will
+             be generated.
+    '''
     if xterm != colorize.xterm:
         colorize.xterm = xterm
         if xterm:
@@ -12,3 +22,4 @@ def colorize(string, rgb=None, ansi=None,bg=None,ansi_bg=None,xterm=True):
 
 colorize.xterm = True
 colorize.cmap = XTermColorMap()
+
