5a72b162b55c597a101e5e16638bc28f5087caa0
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-12-02 14:50:26 +0100

    - Avoids re-creating the colorMap object at every call

--- xtermcolor/__init__.py
+++ xtermcolor/__init__.py
@@ -1,8 +1,14 @@
 from xtermcolor.ColorMap import XTermColorMap, VT100ColorMap
 
+
 def colorize(string, rgb=None, ansi=None,bg=None,ansi_bg=None,xterm=True):
-    if xterm:
-        cmap = XTermColorMap()
-    else:
-        cmap = VT100ColorMap()
-    return cmap.colorize(string,rgb,ansi,bg,ansi_bg)
+    if xterm != colorize.xterm:
+        colorize.xterm = xterm
+        if xterm:
+            colorize.cmap = XTermColorMap()
+        else:
+            colorize.cmap = VT100ColorMap()
+    return colorize.cmap.colorize(string,rgb,ansi,bg,ansi_bg)
+
+colorize.xterm = True
+colorize.cmap = XTermColorMap()
