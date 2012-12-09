332faf0482bff559e6105de89ee8739b9e7b8404
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-12-09 11:29:00 +0100

    works on xterm-256

--- xtermcolor/__init__.py
+++ xtermcolor/__init__.py
@@ -29,7 +29,7 @@ def colorize(string, rgb=None, ansi=None,bg=None,ansi_bg=None,fd=1):
         colorize.init = True
         colorize.is_term = isatty(fd)
         if 'TERM' in environ:
-            if environ['TERM'] == 'xterm':
+            if environ['TERM'].startswith('xterm'):
                 colorize.cmap = XTermColorMap()
             elif environ['TERM'] == 'vt100':
                 colorize.cmap = VT100ColorMap()
